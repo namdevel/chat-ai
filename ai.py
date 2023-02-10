4fa72d93d193e0c7637a34e998c6f996e8b19bdf
几句レし <62225978+namdevel@users.noreply.github.com>
2023-02-11 03:34:22 +0700

    init : create ai

---
+++ ai.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import uuid
+from colorama import Fore, init
+
+init(autoreset=True)
+
+url = "https://api.nam.icu/v1/conversation"
+
+chat = []
+
+chatId = uuid.uuid4()
+
+while True:
+    question = input(f"{Fore.CYAN}You{Fore.RESET}: ")
+    post = {
+        "chat_id": str(chatId),
+        "prompt": question,
+        "chat_data": json.dumps(chat),
+    }
+    answer = ""
+    res = requests.post(url, data=post)
+    data = json.loads(res.text)
+    answer = data["answer"]
+    print(f"{Fore.GREEN}HEUSC{Fore.RESET}:{Fore.LIGHTYELLOW_EX}{answer}")
+    chat.append({"question": question, "answer": answer})
